b50ad4c5c1a107e8c31ddf924dc3355a50c7b617
Huy Hoang Hoang <u7671528@anu.edu.au>
2023-11-18 16:25:15 +1100

    Add ignore file
    -Ignore auto-generated pycache directories

--- Titanic/main.py
+++ Titanic/main.py
@@ -9,4 +9,4 @@ sys.path.insert(0, PROJ_DIR)
 from utils.files import DataFile
 
 if __name__ == '__main__':
-    file = DataFile(os.path.join(PAR_DIR, 'data/train.csv'))
+    train_file = DataFile(os.path.join(PAR_DIR, 'data/train.csv'))
